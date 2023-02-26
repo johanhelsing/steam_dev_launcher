9b25d41f3a4448456868069095ea8c97a4a85757
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2023-02-26 13:00:20 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::{
+    env,
+    fs::File,
+    process::{Command, Stdio},
+};
+
+fn main() {
+    let stdout = Stdio::from(File::create("stdout.txt").unwrap());
+    let stderr = Stdio::from(File::create("stderr.txt").unwrap());
+
+    let mut args = env::args();
+    let _ = args.next(); // ignore first, (this is us)
+    let command = args.next().unwrap();
+
+    // println!("{args:?}");
+    let status = dbg!(Command::new(command)
+        .args(args)
+        .stdout(stdout)
+        .stderr(stderr))
+    .status()
+    .expect("io error while running command");
+
+    std::process::exit(status.code().unwrap());
+    // todo return status
+}
